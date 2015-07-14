b8269bcea679d5d85a49028dd872c91a66048331
xiaohehu <xiaohe.hu@bos-mini-3.local>
2015-07-14 13:33:16 -0400

    Initial Commit

---
+++ SceneKitDaeFile/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SceneKitDaeFile
+//
+//  Created by Xiaohe Hu on 7/14/15.
+//  Copyright (c) 2015 Xiaohe Hu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ SceneKitDaeFile/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SceneKitDaeFile
+//
+//  Created by Xiaohe Hu on 7/14/15.
+//  Copyright (c) 2015 Xiaohe Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
